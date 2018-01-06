c0c998aae8d198fa211304cc251c96a292bc4be6
Thomas Lombart <t.lombart97@gmail.com>
2018-01-06 12:33:50 +0100

    Add helmet and CORS

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import path from 'path';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import passport from 'passport';
+import helmet from 'helmet';
 
 /* CONFIG IMPORT */
 import config from './config';
@@ -19,10 +20,19 @@ import jwtLogin from './config/passport';
 
 const app = express();
 
+
+app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
 
 app.use(passport.initialize());
 passport.use(jwtLogin);

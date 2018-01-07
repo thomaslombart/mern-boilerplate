2b54ebf1c21bbfdf85ed6e034817fc6b6fd3856c
Thomas Lombart <t.lombart97@gmail.com>
2018-01-07 13:31:03 +0100

    Add missing ADD_FLASH_MESSAGE

--- client/src/actions/flash.js
+++ client/src/actions/flash.js
@@ -1,3 +1,4 @@
+export const ADD_FLASH_MESSAGE = 'ADD_FLASH_MESSAGE';
 export const REMOVE_FLASH_MESSAGE = 'REMOVE_FLASH_MESSAGE';
 
 export const removeFlashMessage = (id) => ({type: REMOVE_FLASH_MESSAGE, id});

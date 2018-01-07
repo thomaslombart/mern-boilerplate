249277bc5314d78433f789fcca2dac97cc470011
Thomas Lombart <t.lombart97@gmail.com>
2018-01-07 17:21:21 +0100

    Comment out function

--- server/middlewares/index.js
+++ server/middlewares/index.js
@@ -1,9 +1,10 @@
 import passport from 'passport';
 
 export const requireAuth = passport.authenticate('jwt', { session: false });
-
+/*
 export const isModerator = (req, res, next) => {
     if (req.user.isModerator) {
         return next();
     }
 }
+*/

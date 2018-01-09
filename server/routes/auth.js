5d078f00f405117af58fc77e5753bbf4862024bc
Thomas Lombart <t.lombart97@gmail.com>
2018-01-09 15:12:22 +0100

    Add express-validator support

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,13 @@
 import express from 'express';
 import passport from 'passport';
 import jwt from 'jsonwebtoken';
+import {
+    check,
+    validationResult
+} from 'express-validator/check';
+import {
+    matchedData
+} from 'express-validator/filter';
 
 import User from '../models/user';
 import config from '../config';
@@ -11,27 +18,63 @@ const generateJWT = user => jwt.sign(user, config.jwt.secret);
 
 /**
  * Tries to signin a user
- * 
+ *
  * If the user has signed in succesfully, an object containing a token is sent back to the user
  */
-router.post('/signin', (req, res, next) => {
-    const username = req.body.username.trim();
-    const password = req.body.password;
+router.post('/signin', [
+    check('username')
+    .isLength({
+        min: 2
+    }).withMessage('must have at least 2 characters')
+    .trim(),
+    check('password').isLength({
+        min: 4
+    }).withMessage('must have at least 4 characters')
+    .trim()
+], (req, res, next) => {
+    const errors = validationResult(req);
 
-    User.findOne({
+    if (!errors.isEmpty()) {
+        return res
+            .status(422)
+            .json(errors.mapped())
+    }
+
+    const {
+        username,
+        password
+    } = matchedData(req);
+
+    User
+        .findOne({
             username
         })
         .then(user => {
             if (!user) {
-                return res.status(401).json({
-                    error: 'No user was found'
-                });
+                return res
+                    .status(401)
+                    .json({
+                        username: {
+                            msg: 'No user was found',
+                            param: 'username'
+                        }
+                    });
             } else {
-                let token = generateJWT({id: user.id, username});
+                let token = generateJWT({
+                    id: user.id,
+                    username
+                });
                 return user.validPassword(password) ?
-                    res.status(200).json(token) :
-                    res.status(403).json({
-                        error: 'Wrong password',
+                    res
+                    .status(200)
+                    .json(token) :
+                    res
+                    .status(403)
+                    .json({
+                        password: {
+                            msg: 'Invalid password',
+                            param: 'password'
+                        }
                     });
             }
         })
@@ -39,36 +82,59 @@ router.post('/signin', (req, res, next) => {
 
 /**
  * Tries to signup a user
- * 
+ *
  * If the user has signed up succesfully, an object containing a token is sent back to the user
  */
-router.post('/signup', function (req, res, next) {
-    const username = req.body.username.trim();
-    const password = req.body.password;
-    const passwordVerification = req.body.passwordVerification;
+router.post('/signup', [
+    check('username')
+    .isLength({
+        min: 2
+    }).withMessage('must have at least 2 characters')
+    .custom(value => {
+        return User.findOne({
+                username: value
+            })
+            .then(user => {
+                if (user) {
+                    throw new Error('Username already taken');
+                } else {
+                    return true;
+                }
+            })
+    })
+    .trim(),
+    check('password').isLength({
+        min: 4
+    }).withMessage('must have at least 4 characters')
+    .trim(),
+    check('passwordVerification').custom((value, {
+        req
+    }) => value === req.body.password).withMessage('Both passwords should be the same')
+], (req, res, next) => {
+    const errors = validationResult(req);
 
-    if (password !== passwordVerification) {
-        return res.status(401).json({
-            error: 'Passwords are different.'
-        });
+    if (!errors.isEmpty()) {
+        return res
+            .status(422)
+            .json(errors.mapped());
     }
 
-    User.findOne({
+    const {
+        username,
+        password,
+        passwordVerification
+    } = matchedData(req);
+
+    let userToCreate = new User({
+        username,
+        password
+    });
+    userToCreate
+        .save()
+        .then(user => res.json(generateJWT({
+            id: user.id,
             username
-        })
-        .then(user => {
-            if (user) {
-                return res.status(401).json({
-                    error: `That username is already used`,
-                });
-            }
-            let userToCreate = new User({
-                username,
-                password
-            });
-            userToCreate.save(user)
-                .then(user => res.json(generateJWT({id: user.id, username})));
-        });
+        })));
 });
 
 export default router;
